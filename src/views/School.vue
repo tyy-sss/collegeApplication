f6a2154be69c56012c9a7f3fe6a58e72444e0bfe
tyy-sss <3127023395@qq.com>
2024-03-06 19:29:01 +0800

    删除不必要的界面

--- src/views/School.vue
+++ src/views/School.vue
@@ -15,20 +15,17 @@
     <div class="middle">
       <!-- 学校管理 -->
       <school-manage v-if="showItem === 1" />
-      <!-- 学校信息 -->
-      <school-table v-if="showItem === 2" />
       <!-- 志愿地区管理 -->
-      <volunteer-address v-if="showItem === 3" />
+      <volunteer-address v-if="showItem === 2" />
       <!-- 志愿规则说明 -->
-      <volunteer-rule-explain v-if="showItem === 4" />
+      <volunteer-rule-explain v-if="showItem === 3" />
       <!-- 高考科目管理 -->
-      <subject-manage v-if="showItem === 5" />
+      <subject-manage v-if="showItem === 4" />
     </div>
   </div>
 </template>
 <script setup>
 import schoolManage from "@/components/school/school-manage.vue";
-import schoolTable from "@/components/school/school-table.vue";
 import volunteerRuleExplain from "@/components/school/volunteer-rule-explain.vue";
 import volunteerAddress from "@/components/school/volunteer-address.vue";
 import subjectManage from "@/components/school/subject-manage.vue";
@@ -40,12 +37,6 @@ const topChoose = reactive([
     text: "学校信息管理",
     color: "RGBA(117,123,242,0.7)",
     shadow: " 8px 8px 5px RGBA(117,123,242,0.3)",
-  },
-  {
-    img: require("@/assets/image/school-information.png"),
-    text: "学校信息录入",
-    color: "RGBA(255,124,51,0.7)",
-    shadow: " 8px 8px 5px RGBA(255,124,51,0.3)",
   },
   {
     img: require("@/assets/image/volunteer-address.png"),
